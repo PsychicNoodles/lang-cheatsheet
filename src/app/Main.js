d12d6dd3e2d143a601cc6b9a43b197514a2e86c8
Birnbaum, Mattori <Birnbaum.Mattori@principal.com>
2016-06-14 16:26:48 -0500

    implemented basic navigation drawer and router

--- src/app/Main.js
+++ src/app/Main.js
@@ -2,17 +2,28 @@
  * In this file, we create a React component
  * which incorporates components providedby material-ui.
  */
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
+import {withRouter} from 'react-router'
 import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer'
-import MenuItem from 'material-ui/MenuItem'
+import {List, ListItem, MakeSelectable} from 'material-ui/List'
+
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+const SelectableList = MakeSelectable(List)
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {open: false}
+  }
+
+  onListChange(evt, value) {
+    this.props.router.push(value)
+    this.setState({
+      open: false
+    })
   }
 
   render() {
@@ -25,8 +36,20 @@ class Main extends Component {
           />
           <Drawer docked={false} open={this.state.open}
                   onRequestChange={(open) => this.setState({open})}>
-            <MenuItem>Item 1</MenuItem>
-            <MenuItem>Item 2</MenuItem>
+            <ListItem primaryText="Home" onTouchTap={(evt) => this.onListChange(evt, '/')}/>
+            <SelectableList onChange={this.onListChange.bind(this)}>
+              <ListItem primaryText="한국어" secondaryText="Korean"
+                        primaryTogglesNestedList={true}
+                        nestedItems={[
+                          <ListItem primaryText="Consonant Pronunciation Rules"
+                                    value="consonant-rules"/>
+                        ]}/>
+              <ListItem primaryText="日本語" secondaryText="Japanese"
+                        primaryTogglesNestedList={true}
+                        nestedItems={[
+                          <ListItem primaryText="Coming Soon™"/>
+                        ]}/>
+            </SelectableList>
           </Drawer>
         </div>
       </MuiThemeProvider>
@@ -34,4 +57,4 @@ class Main extends Component {
   }
 }
 
-export default Main;
+export default withRouter(Main)
